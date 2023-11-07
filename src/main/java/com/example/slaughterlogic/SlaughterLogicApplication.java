3c7c72458d1a6fa20932a87cf6b841f93c528b1c
KBence <kabaly.bence@gmail.com>
2023-11-07 15:24:07 +0100

    bam

---
+++ src/main/java/com/example/slaughterlogic/SlaughterLogicApplication.java
@@ -0,0 +1,13 @@
+package com.example.slaughterlogic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlaughterLogicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SlaughterLogicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/slaughterlogic/SlaughterLogicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.slaughterlogic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlaughterLogicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
