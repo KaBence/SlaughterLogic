0557920fc23609ab4b0d192b5338e2d4a8983ad6
barbyabr <116642519+barbyabr@users.noreply.github.com>
2023-11-13 21:39:15 +0100

    HalfAnimalPackage shit

---
+++ src/main/java/Shared/HalfAnimalPackage.java
@@ -0,0 +1,27 @@
+package Shared;
+
+public class HalfAnimalPackage {
+    private int  half_package_id;
+
+    public HalfAnimalPackage()
+    {
+
+    }
+
+    public HalfAnimalPackage(int half_package_id) {
+        this.half_package_id = half_package_id;
+    }
+
+    public int getHalf_package_id() {
+        return half_package_id;
+    }
+
+    public void setHalf_package_id(int half_package_id) {
+        this.half_package_id = half_package_id;
+    }
+
+    public String toString()
+    {
+        return "Package type: Half Animal Package; Id: "+half_package_id;
+    }
+}
